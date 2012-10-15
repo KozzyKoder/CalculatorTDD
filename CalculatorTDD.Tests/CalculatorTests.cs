6c4c1019feac9b3e3d7fd1b1b67af36a2784b678
Vladimir.Pestov <pestovvladimir@gmail.com>
2012-10-15 12:05:15 +0500

    Added tests project

---
+++ CalculatorTDD.Tests/CalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CalculatorTDD.Tests
+{
+    
+    public class CalculatorTDDTests
+    {
+        [Fact]
+        public void CalculatorExpressionShouldNotBeEmotyOrNull()
+        {
+            const string emptyExpression = "";
+            const string nullExpression = null;
+            var calculator = new Calculator();
+
+            Assert.Throws(typeof(ArgumentException), () => calculator.Calculate(emptyExpression));
+            Assert.Throws(typeof(ArgumentException), () => calculator.Calculate(nullExpression));
+        }
+    }
+}
---
+++ CalculatorTDD/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculatorTDD
+{
+    public class Calculator
+    {
+        public int Calculate(string expression)
+        {
+            if (string.IsNullOrEmpty(expression))
+            {
+                throw new ArgumentException();
+            }
+
+            return 0;
+        }
+    }
+}
